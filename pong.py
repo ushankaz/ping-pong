17411994dc898eadc584c3f4b0b659ea0e7cb85f
ushankaz <dupakdima11@gmail.com>
2025-12-03 18:35:45 +0300

    Add files via upload

---
+++ pong.py
@@ -0,0 +1,52 @@
+from pygame import *
+win = display.set_mode((500, 500))
+win.fill((255, 255, 255))
+FPS = 60
+clock = time.Clock()
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 420:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 420:
+            self.rect.y += self.speed
+
+finish = False
+while finish == False:
+    for e in event.get():
+        if e.type == QUIT:
+            finish = True
+
+
+
+
+
+
+
+
+
+
+    display.update()
+    clock.tick(FPS)
+
+
+
