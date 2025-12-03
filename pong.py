5df814fc0d2977d1c75d718309dcb3ca8bcc283d
ushankaz <dupakdima11@gmail.com>
2025-12-03 18:57:58 +0300

    Add files via upload

--- pong.py
+++ pong.py
@@ -30,21 +30,39 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 420:
             self.rect.y += self.speed
 
+
+player_left = Player('racket.png', 30, 200, 20, 70, 5)
+player_right = Player('racket.png', 450, 200, 20, 70, 5)
+ball = GameSprite('tenis_ball.png', 250, 250, 50, 50, 6)
+
+speed_x = 3
+speed_y = 2
 finish = False
 while finish == False:
     for e in event.get():
         if e.type == QUIT:
             finish = True
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+    if ball.rect.x >= 450 or ball.rect.x <= 0:
+        speed_x *= -1
+    if ball.rect.y >= 450 or ball.rect.y <= 0:
+        speed_y *= -1
+
+    if sprite.collide_rect(player_left, ball):
+        
+        speed_x *= -1
+    if sprite.collide_rect(player_right, ball):
+        
+        speed_x *= -1
 
 
-
-
-
-
-
-
-
-
+    win.fill((255, 255, 255))
+    player_left.update_l()
+    player_right.update_r()
+    player_left.reset()
+    player_right.reset()
+    ball.reset()
     display.update()
     clock.tick(FPS)
 
