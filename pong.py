afbf7836208cde9149a8662dd1ef358887890b79
ushankaz <dupakdima11@gmail.com>
2025-12-07 10:26:51 +0300

    Add files via upload

--- pong.py
+++ pong.py
@@ -3,6 +3,7 @@ win = display.set_mode((500, 500))
 win.fill((255, 255, 255))
 FPS = 60
 clock = time.Clock()
+font.init()
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
@@ -35,34 +36,46 @@ player_left = Player('racket.png', 30, 200, 20, 70, 5)
 player_right = Player('racket.png', 450, 200, 20, 70, 5)
 ball = GameSprite('tenis_ball.png', 250, 250, 50, 50, 6)
 
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+game = False
 speed_x = 3
 speed_y = 2
 finish = False
-while finish == False:
+while game == False:
     for e in event.get():
         if e.type == QUIT:
+            game = True
+    if finish != True:
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.x >= 450:
             finish = True
-    ball.rect.x += speed_x
-    ball.rect.y += speed_y
-    if ball.rect.x >= 450 or ball.rect.x <= 0:
-        speed_x *= -1
-    if ball.rect.y >= 450 or ball.rect.y <= 0:
-        speed_y *= -1
+            win.blit(lose2, (200, 200))
+            display.update()
+        if ball.rect.x <= 0:
+            finish = True
+            win.blit(lose1, (200, 200))
+            display.update()
+        if ball.rect.y >= 450 or ball.rect.y <= 0:
+            speed_y *= -1
 
-    if sprite.collide_rect(player_left, ball):
+        if sprite.collide_rect(player_left, ball):
+            
+            speed_x *= -1
+        if sprite.collide_rect(player_right, ball):
+            
+            speed_x *= -1
+
         
-        speed_x *= -1
-    if sprite.collide_rect(player_right, ball):
-        
-        speed_x *= -1
-
-
-    win.fill((255, 255, 255))
-    player_left.update_l()
-    player_right.update_r()
-    player_left.reset()
-    player_right.reset()
-    ball.reset()
+        win.fill((255, 255, 255))
+        player_left.update_l()
+        player_right.update_r()
+        player_left.reset()
+        player_right.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
 
